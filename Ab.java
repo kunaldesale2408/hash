e2b3cc211abba90ac88aa5009ac66cce05e1ff35
kunaldesale2408 <142870456+kunaldesale2408@users.noreply.github.com>
2023-09-16 11:24:52 +0530

    Add files via upload

---
+++ Ab.java
@@ -0,0 +1,15 @@
+public class Ab
+{
+int i=15;
+void increment(Ab.ob1)
+{
+obj.i++;
+}
+public static void main(string[] args)
+{
+Ab.obj=new Ab();
+System.out.println("Before incrementing value of i="+obj.i);
+obj.increment(obj);
+System.out.println("After incrementing value of i="+obj.i);
+}
+}
